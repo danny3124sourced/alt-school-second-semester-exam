20e766773b55ebd3724d352393bf97ceb6123496
Daniel Monte <92448293+danny3124sourced@users.noreply.github.com>
2022-11-06 10:00:06 -1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {Link, BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Repo from './Repo';
 import Error from './Error';
 import Nav from './Nav';
 import List from './List';
 function App() {
   const [repo, setrepo] = useState();
-  const [index, setIndex] = useState(1);
   useEffect(() => {
     const fetch = async () => {
       const res = await axios.get("https://api.github.com/users/danny3124sourced/repos")
